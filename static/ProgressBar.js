54da1c8b9d98cb87c8ea8c5c4f5fa4cef772ca5f
bleeptrack <mail@bleeptrack.de>
2024-09-03 15:17:13 +0200

    remove 'undefined' from progressbar

--- static/ProgressBar.js
+++ static/ProgressBar.js
@@ -54,7 +54,9 @@ export class ProgressBar extends HTMLElement {
 	}
 	
 	setPercentage(percentage, label){
-		this.shadow.getElementById("label").innerHTML = label
+		if(label){
+			this.shadow.getElementById("label").innerHTML = label
+		}
 		for(let rule of this.shadow.styleSheets[this.shadow.styleSheets.length-1].cssRules){
 			if(rule.selectorText  == "#progress::after" ){
 				rule.style.width = `calc(${percentage}% - 1px)`
